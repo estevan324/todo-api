5724fb1b83b5708d37b580aaeecf7d0a6e49b09e
Estevan Hernandes Silvade Oliveira <estevanhernandes1@gmail.com>
2025-01-11 20:53:43 -0300

    feat: generic repository

---
+++ ToDo.Application/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Infrastructure.Data.Context;
+using ToDo.Infrastructure.Repositories;
+
+namespace ToDo.Application.Repositories;
+
+public class Repository<TData>(ApplicationDbContext context) : IRepository<TData> where TData : class
+{
+    public async Task<IList<TData>> GetAllAsync() 
+        => await context.Set<TData>().ToListAsync();
+
+    public async Task<TData?> GetByIdAsync(Guid id)
+        => await context.Set<TData>().FindAsync(id);
+
+    public async Task SaveAsync(TData data)
+    {
+        if (context.Entry(data).State is EntityState.Detached)
+            await context.Set<TData>().AddAsync(data);
+        else
+            context.Set<TData>().Update(data);
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            context.Set<TData>().Remove(entity); 
+            await context.SaveChangesAsync(); 
+        }
+    }
+}
---
+++ ToDo.Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using ToDo.Infrastructure.Data.Context;
+
+namespace ToDo.Infrastructure.Repositories;
+
+public interface IRepository<T>
+{
+    Task<IList<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(Guid id);
+    Task SaveAsync(T data);
+    Task DeleteAsync(Guid id);    
+}
